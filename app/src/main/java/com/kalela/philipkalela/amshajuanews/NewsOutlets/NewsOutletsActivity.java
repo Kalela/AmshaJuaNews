40277b5930b0bba4125d7c9fb645c0c4618b79d8
Kalela <philipkalela@gmail.com>
2019-08-23 16:20:02 +0300

    [feature] Set up PBF and MVP architecture:
    - Set up PBF folder structure and MVP architecture
    
    [Delivers: #168003842]

---
+++ app/src/main/java/com/kalela/philipkalela/amshajuanews/NewsOutlets/NewsOutletsActivity.java
@@ -0,0 +1,15 @@
+package com.kalela.philipkalela.amshajuanews.NewsOutlets;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.kalela.philipkalela.amshajuanews.R;
+
+public class NewsOutletsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news_outlets);
+    }
+}
